0a47047b7fc79ba0796f882bdd9bc1a7ecff7948
Anjita Chatterjee <c04anjita@gmail.com>
2025-07-24 22:13:29 +0530

    Fix: updated host and port for Railway

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def generate():
 
     return send_file(output, download_name=f"certificate_{name}.pdf", as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
