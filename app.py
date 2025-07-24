e21d63407d714527cba7ed0aa6384fff80eb3819
Anjita Chatterjee <c04anjita@gmail.com>
2025-07-24 23:42:10 +0530

    Ready for Deployment

--- app.py
+++ app.py
@@ -46,7 +46,8 @@ def generate():
     output.seek(0)
 
     return send_file(output, download_name=f"certificate_{name}.pdf", as_attachment=True)
-
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
